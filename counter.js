363839f1e88ca23974e570acdcdd11a4391b8e2b
Paccony <90724617+Paccony@users.noreply.github.com>
2022-05-05 22:38:07 +0200

    Add files via upload

---
+++ counter.js
@@ -0,0 +1,32 @@
+// set initial count
+let count = 0;
+// select value and buttons
+let value = document.querySelector('#value');
+let btns = document.querySelectorAll('.btn');
+
+btns.forEach(function (btn) {
+    btn.addEventListener("click", function(e) {
+        const styles = e.currentTarget.classList;
+        if(styles.contains('decrease')) {
+            count--;
+        }
+       
+       else if (styles.contains('increase')) {
+           count ++;
+       }
+       else{
+           count = 0;
+       }
+       if (count > 0) {
+           value.style.color = 'red';
+       }
+       if (count<0) {
+           value.style.color ='blue';
+       }
+       if(count ===0) {
+           value.style.color ='purple';
+       }
+
+       value.textContent = count;
+    });
+});
